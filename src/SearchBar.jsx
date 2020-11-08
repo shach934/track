1c49a0eb2c0c40e2e9c425c995667267a9f6e214
shach934 <tjukid@gmail.com>
2020-11-08 01:22:40 +0100

    search works now

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -4,60 +4,62 @@ import "./header.css";
 function Search(){
     let [parcelID, setParcelID] = useState("");
     let [data, setData] = useState([]);
-    let [found, setFound] = useState([]);
+    let [found, setFound] = useState();
+    let [searched, setSearch] = useState(false);
     
-    useEffect(() => {
+    useEffect((data) => {
         const urlAPI = 'https://my.api.mockaroo.com/orders.json?key=e49e6840';
         fetch(urlAPI).then(response => response.json())
-        .then( item => console.log("this is the data: " + item))
-        .then( item => setData(item))
+        .then( item => setData(item));
     }, []);
 
     const getInput = (event) => {
         const inputID = event.target.value;
         setParcelID(inputID);
-        console.log("The input Parcel ID is: " + parcelID);
     }
 
-    const handleSearch = (event) =>{
-        const inputID = parcelID.padStart(4, "0");
-        const parcel = data.filter(item => item.id === inputID);
+    const handleSearch = () =>{
+        const parcel = data.filter(item => item.id === parcelID);
         setFound(parcel);
         PresentParcel(found);
+        setSearch(true);
     }
 
     const PresentParcel =()=> {
-        console.log("the package found is: " + found);
-        if(found.length === 0){
-            return (
-            <div>
-                The Package is NOT found, please check the parcel ID and try again.
-            </div>);
-        }else
-        {   
-            return (
+        if(searched){
+            data.map(item => console.log(item.id));
+            console.log(found);
+            if(found.length === 0){
+                return (
                 <div>
-                    <label>ID: </label>
-                    <p>{found.id}</p>
-                    <label>ETA: </label>
-                    <p>{found.eta}</p>
-                    <label>Location: </label>
-                    <p>{found.location_name}</p>
-                    <label>status: </label>
-                    <p>{found.status}</p>
-                    <label>ID: </label>
-                    <p>{found.id}</p>
-                </div>
-            )
+                    The Package {parcelID} is NOT found, please check the parcel ID and try again.
+                </div>);
+            }else
+            {   
+                return (
+                    <div>
+                        <label>ID: </label>
+                        <p>{found[0].id}</p>
+                        <label>ETA: </label>
+                        <p>{found[0].eta}</p>
+                        <label>Location: </label>
+                        <p>{found[0].location_name}</p>
+                        <label>status: </label>
+                        <p>{found[0].status}</p>
+                    </div>
+                )
+            }
+        }else{
+            return null;    
         }
+        
     }
 
     return (
         <div className="header">
             <legend>Enter the Parcel ID(4 digits number):</legend>
-            <input value = {parcelID} onChange = {getInput}/>
+            <input placeholder="Enter parcel ID" value = {parcelID} onChange = {getInput}/>
             <button onClick = {handleSearch}>Search</button>
-            <p>This is the Parcel ID you are searching: {parcelID}</p>
             <PresentParcel />
         </div>
     );
